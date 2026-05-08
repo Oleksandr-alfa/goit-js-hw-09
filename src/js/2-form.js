acbd1ba7894c329625c6e856ae7d48f7823ad5c8
Oleksandr-alfa <a_kos_76_@ukr.net>
2026-05-09 02:25:01 +0300

    final version homework 9

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {
+    email: '',
+    message: ''
+}
+ const form = document.querySelector(`.feedback-form`);
+form.addEventListener(`input`, onFormInput);
+form.addEventListener(`submit`, onFormSubmit);
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+if (savedData) {
+    form.elements[`user-email`].value = savedData.email || '';
+    form.elements[`user-message`].value = savedData.message || '';
+
+     formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+}
+function onFormInput() {
+    formData.email = form.elements[`user-email`].value.trim();
+    formData.message = form.elements[`user-message`].value.trim();
+ localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit() {
+    event.preventDefault();
+    const email = form.elements[`user-email`].value.trim();
+    const message = form.elements[`user-message`].value.trim();
+    if (email === '' || message === '') {
+        alert('Fill please all fields');
+        return;
+    } else {
+        formData.email = email;
+        formData.message = message;
+       
+    }
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+   
